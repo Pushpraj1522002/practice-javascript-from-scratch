5f6b139e0c207039375d3661086831af84dc7b2d
Pushpraj1522002 <106546262+Pushpraj1522002@users.noreply.github.com>
2024-01-24 07:57:33 +0000

    dataTypes

---
+++ first/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all code as newer verstion.
+var hello = "hello"
+console.log(hello);
+
+// alert(3+3) // it will not work in nodejs in this way, if we run this line of code in browser console it will run just fine.
+console.log("please"); console.log("do not do this") //it is not the good practice because it is not readable
+
+//semicolon is use to terminate the current statement. It is the good practice.
+
+// primitive datatypes ....
+//number 
+//bigint
+//string : " "
+//boolean : true/false
+//null : standalone value
+//undefined
+//symbol : uniqueness of the component
+
+//user defined datatypes.
+// object
+
+console.log(typeof undefined); // undefined.
+console.log(typeof null); // object
+
+
+
+
--- first/first_file.js
+++ first/first_file.js
@@ -1,1 +1,23 @@
-console.log("hello")
+//console.log("hello")
+const accountId = 14444
+let accountEmail = "hello@gmail.com"
+
+
+//the 'var' keyword was used before, but it has scope problem. It's better to use the 'let' keywords instead.
+//prefer not to use the var
+var accountPassword = "1234"
+accountCity = "Pune"
+
+accountEmail = "hello123@gmail.com"
+accountPassword = "123121"
+accountCity = "Mumbai"
+
+let accountState;
+
+// accountId = 4
+/* console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity); */
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
